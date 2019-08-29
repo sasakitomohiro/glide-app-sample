2ec6008a3bb64be62e80749a749c07553655f2d5
t0mat0sk <t0m0.piii.12.07sk@gmail.com>
2019-08-29 17:17:19 +0900

    first commit

---
+++ app/src/main/java/monster/sasakisan/glide_app_sample/MainActivity.kt
@@ -0,0 +1,22 @@
+package monster.sasakisan.glide_app_sample
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import monster.sasakisan.glide_app_sample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        GlideApp
+            .with(this)
+            .load(R.drawable.ic_launcher_foreground)
+            .into(binding.imageView)
+    }
+}
---
+++ app/src/main/java/monster/sasakisan/glide_app_sample/SampleAppGlideModule.kt
@@ -0,0 +1,8 @@
+package monster.sasakisan.glide_app_sample
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class SampleAppGlideModule : AppGlideModule() {
+}
